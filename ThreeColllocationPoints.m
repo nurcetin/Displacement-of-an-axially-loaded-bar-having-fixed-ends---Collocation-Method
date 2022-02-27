81ab85dabb2abd90a61a485a790182d7b705e41b
nurcetin <nur.cetin@metu.edu.tr>
2022-02-27 18:53:31 +0300

    MATLAB

--- ThreeColllocationPoints.m
+++ ThreeColllocationPoints.m
@@ -16,16 +16,18 @@ u = phizero+c1*phione+c2*phitwo+c3*phithree;
 % Governing Equation
 eqn=diff(E*A*(diff(u)))+3*x;
 % Collocation Points
-x1=2/3;
-x2=2*2/3;
+x1=2/4;
+x2=2*2/4;
+x3=3*2/4;
 % Weighted Residual Method
 eqn=subs(eqn,x1)==0;
 eqn2=subs(eqn,x2)==0;
 eqn3=subs(eqn,x3)==0;
 % Finding Coefficients c1 and c2
-sol = solve([eqn1, eqn2], [c1, c2]);
+sol = solve([eqn1, eqn2, eqn3], [c1, c2, c3]);
 c1 = sol.c1;
 c2 = sol.c2;
+c3 = sol.c3;
 % Approximate Solution for Displacement u(x)
 u = phizero+c1*phione+c2*phitwo+c3*phithree;
 % Approximate Solution for N(x)
